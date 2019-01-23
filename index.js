1913ab364e41b21fbc67ead22382c2006533ac6f
jaeha <years6666@gmail.com>
2019-01-24 00:20:27 +0900

    using render ... query

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get("/", (req, res)=> {
     res.send("Hello Node ");
 })
 
+app.get("/admin",function(req,res){
+    var name = req.query.name
+    //url 에 있는 주소를 보고 name 에 집어넣을 수 있음
+    res.render("")
+
+})
+
 
 app.listen(3000 , () =>{
     console.log("server running");
