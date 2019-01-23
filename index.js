bfad1d1358296d9b4052221e798b2ebbe953b40f
jaeha <years6666@gmail.com>
2019-01-24 00:24:54 +0900

    npm install pug and make views folder and main.pug

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine","pug")
+app.set("views","./views")
+
+
+
 app.get("/", (req, res)=> {
     res.send("Hello Node ");
 })
